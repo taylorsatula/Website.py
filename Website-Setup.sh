658c65ea8eafc7b5ca5d821a48ebc4524d2b2e5f
Taylor Satula <Taylor@Taylors-MacBook.local>
2014-03-15 17:40:47 -0400

    waiting on the world
    I added some sleeps… don’t judge me.

--- Website-Setup.sh
+++ Website-Setup.sh
@@ -1,20 +1,71 @@
 #!/bin/bash
 ## DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+## Yes, I realize there are a lot of sleeps in here. It doesn't look fancy if it takes a tenth of a second.
+## If you want it to be instant just remove all the lines involving sleep and also the from "Generating" to "echo -ne '\n'"
+## Shhh.... don't tell anyone.
 
 
 echo "Please enter folder name: "
 read foldername
 echo ""
 
+echo "Generating..."
+echo -ne '###                              (10%)\r'
+sleep 0.5
+echo -ne '######                           (20%)\r'
+sleep 0.5
+echo -ne '#########                        (30%)\r'
+sleep 0.5
+echo -ne '############                     (40%)\r'
+sleep 0.5
+echo -ne '###############                  (50%)\r'
+sleep 0.5
+echo -ne '##################               (60%)\r'
+sleep 0.5
+echo -ne '#####################            (70%)\r'
+sleep 0.5
+echo -ne '########################         (80%)\r'
+sleep 0.5
+echo -ne '###########################      (90%)\r'
+sleep 0.5
+echo -ne '##############################   (100%)\r'
+echo -ne '\n'
+
 echo "=-=-=-=-=-=-=-=-=-=-=-=-="
 mkdir -p  "$HOME/Desktop/$foldername"
 echo "Folder Created"
+
+
+sleep 0.1
+
+
 echo "Populating Folder"
 mkdir -p  "$HOME/Desktop/$foldername/images"
+
+
+
+sleep 0.1
+
+
 echo "Images folder created"
 mkdir -p  "$HOME/Desktop/$foldername/javascript"
+
+
+sleep 0.1
+
+
 echo "Javascript folder created"
+
+
+sleep 0.1
+
+
 echo "Populating reset.css with Reset CSS"
+
+
+sleep 0.1
+
+
 cat <<EOF >"$HOME/Desktop/$foldername/reset.css"
 
 ## CSS RESET DOCUMENT SHOULD GO HERE BEFORE A RELEASE PUSH
@@ -38,6 +89,11 @@ cat <<EOF >"$HOME/Desktop/$foldername/index.html"
 ## SINCE I CANNOT DO CODE FOLDING IN SHELL SCRIPTS
 
 EOF
+
+
+sleep 0.5
+
+
 echo -e "\033[0;32mOperation Compete\033[00m"
 echo "=-=-=-=-=-=-=-=-=-=-=-=-="
 echo ""
