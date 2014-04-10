b11168969e53b088fdebfc151b9e4364694e73b7
Taylor Satula <Taylor@Taylors-MacBook.local>
2014-04-10 00:22:55 -0400

    Now with Espresso support!
    Now it will automatically open the folder in Espresso if you have it
    installed. Future versions will let you choose a persistent text editor.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 New Project Template
 =============
 
-Full description can be found at project website. It is linked in the header of the repo.
-http://taylorsatula.github.io/Website.sh/
+Setting up new projects sucks. This helps.
+
+When this script is run it creates a folder in the same directory that the script is in and populates it with 
+```foldername/
+foldername/images/
+foldername/javascript/
+foldername/index.html
+foldername/style.css
+```
+By default these are filled with a basic HTML5 page and Eric Meyer's CSS reset.
+The CSS document is even linked to the HTML document automatically.
+
+Before running for the first time you may have to make the script executable by running the
+```
+	chmod +x Website-Setup.sh
+```
+This will set it up so all you have to is call the script from terminal and it'll go about its business.
--- Website-Setup.sh
+++ Website-Setup.sh
@@ -2,7 +2,7 @@
 ## DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ## Yes, I realize there are a lot of sleeps in here. It doesn't look fancy if it takes a tenth of a second.
 ## If you want it to be instant just remove all the lines involving sleep and also the from "Generating" to "echo -ne '\n'"
-## Shhh.... don't tell anyone.  
+## Shhh.... don't tell anyone.
 
 
 echo "Please enter folder name: "
@@ -61,61 +61,67 @@ echo "=-=-=-=-=-=-=-=-=-=-=-=-="
 mkdir -p  "$HOME/Desktop/$foldername"
 echo "Folder Created"
 
-
 sleep 0.1
-
 
 echo "Populating Folder"
+
+sleep 0.1
+
 mkdir -p  "$HOME/Desktop/$foldername/images"
-
-
+echo "Image folder created"
 
 sleep 0.1
 
-
-echo "Images folder created"
 mkdir -p  "$HOME/Desktop/$foldername/javascript"
-
-
-sleep 0.1
-
-
 echo "Javascript folder created"
 
-
 sleep 0.1
 
-
+cat <<EOF >"$HOME/Desktop/$foldername/reset.css"
+html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block;}body{line-height:1;}ol,ul{list-style:none;}blockquote,q{quotes:none;}blockquote:before,blockquote:after,q:before,q:after{content:'';content:none;}table{border-collapse:collapse;border-spacing:0;}
+EOF
 echo "Populating reset.css with Reset CSS"
 
+sleep 0.1
+
+cat <<EOF >"$HOME/Desktop/$foldername/style.css"
+@import "reset.css";
+/* You might want to inline this before release to save a HTTP request */
+
+body {
+	
+}
+EOF
+echo "Populating style.css with basic page and @import"
 
 sleep 0.1
 
-
-cat <<EOF >"$HOME/Desktop/$foldername/reset.css"
-
-## CSS RESET DOCUMENT SHOULD GO HERE BEFORE A RELEASE PUSH
-## I JUST REMOVED IT SO IT WOULD BE EASIER TO WORK WITH
-## SINCE I CANNOT DO CODE FOLDING IN SHELL SCRIPTS
-
-EOF
-echo "Populating style.css with basic page and @import"
-cat <<EOF >"$HOME/Desktop/$foldername/reset.css"
-
-## CSS PAGE DOCUMENT SHOULD GO HERE BEFORE A RELEASE PUSH
-## I JUST REMOVED IT SO IT WOULD BE EASIER TO WORK WITH
-## SINCE I CANNOT DO CODE FOLDING IN SHELL SCRIPTS
-
+cat <<EOF >"$HOME/Desktop/$foldername/index.html"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>$foldername | Skeleton</title>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<header>
+		HEADER CONTENT
+	</header>
+	
+	<main>
+		BODY OF PAGE CONTENT
+	</main>
+	
+	<!-- ANALYTICS TRACKING CODE -->
+</body>
+</html>
 EOF
 echo "Populating index.html with basic HTML5 page"
-cat <<EOF >"$HOME/Desktop/$foldername/index.html"
-
-## INDEX HTML DOCUMENT SHOULD GO HERE BEFORE A RELEASE PUSH
-## I JUST REMOVED IT SO IT WOULD BE EASIER TO WORK WITH
-## SINCE I CANNOT DO CODE FOLDING IN SHELL SCRIPTS
-
-EOF
-
 
 sleep 0.6
 
@@ -124,3 +130,12 @@ echo -e "\033[0;32mOperation Compete!\033[00m"
 echo "=-=-=-=-=-=-=-=-=-=-=-=-="
 echo ""
 echo "Your new project resides at $HOME/Desktop/$foldername/"
+echo ""
+echo ""
+echo "This script is still very young. Send bug reports to: @taylorsatula"
+
+sleep 0.5
+
+if [ -d "/Applications/Espresso.app" ]; then
+open -a /Applications/Espresso.app "$HOME/Desktop/$foldername/"
+fi
